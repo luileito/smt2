372d3d511f042c7adab7db7be7db4d6f8d7ebf3b
luis.leiva.torres <luis.leiva.torres@9911bacc-68cb-11de-89a9-0b20f3e2dceb>
2012-03-24 11:15:30 +0000

    Fixed log downloading. Interacted elements are excluded from CSV/TSV files to save space.

--- trunk/admin/ext/tracking-report/download.php
+++ trunk/admin/ext/tracking-report/download.php
@@ -2,14 +2,17 @@
 // server settings are required - relative path to smt2 root dir
 require_once '../../../config.php';
 
-$where =  !empty($_SESSION['filterquery']) ? $_SESSION['filterquery'] : 
-          !empty($_GET['id'])  ? "id='". $_GET['id'] ."'" : 
-          !empty($_GET['pid']) ? "pid='".$_GET['pid']."'" :
-          !empty($_GET['cid']) ? "cid='".$_GET['cid']."'" :
-          !empty($_GET['ip'])  ? "ip='". $_GET['ip'] ."'" :
-          "1";
-$records = db_select_all(TBL_PREFIX.TBL_RECORDS, "*", $where." ORDER BY sess_date, client_id");
+// ensure that we can create ZIP files
+if (!class_exists('ZipArchive')) die("Your PHP distribution does not support ZipArchive.");
 
+if (isset($_SESSION['filterquery'])) $where = $_SESSION['filterquery'];
+else if (isset($_POST))       $where = "1";
+else if (isset($_GET['id']))  $where = "id='". $_GET['id'] ."'";
+else if (isset($_GET['pid'])) $where = "id='". $_GET['pid'] ."'";
+else if (isset($_GET['cid'])) $where = "id='". $_GET['cid'] ."'";
+else $where = "1"; // default: download all logs
+
+$records = db_select_all(TBL_PREFIX.TBL_RECORDS, "*", $where." ORDER BY sess_date, client_id");
 if (!$records) { die("No logs found matching your criteria!"); }
 
 $format = isset($_POST['format']) ? $_POST['format'] : "csv";
@@ -31,13 +34,15 @@ switch ($format) {
 
 // get column names first
 $headers = db_records(true);
-
 // parse DSV formats
 $dsv = implode(",", $headers);
 foreach ($records as $i => $r) {
   $row = array();
   foreach ($headers as $h) {
-    $row[] = '"'.$r[$h].'"';
+    // exclude interacted elements?
+    if ($h != "hovered" && $h != "clicked") {
+      $row[] = '"'.$r[$h].'"';
+    }
   }
   $dsv .= PHP_EOL.implode($delimiter, $row);
 }
@@ -49,16 +54,19 @@ $zippath = CACHE_DIR.'/'.$zipname;
 $zip = new ZipArchive;
 $res = $zip->open($zippath, ZipArchive::OVERWRITE);
 if (!$res) {
-  die('Cannot create ZIP file.');
+  die('Cannot open ZIP file.');
 }
 // add meta comments
-$zip->setArchiveComment("Downloaded on ".date('l jS \of F Y h:i:s A'));
-$readme  = "Each column's value is delimited by a new line. These are their names:".PHP_EOL;
-$readme .= implode(PHP_EOL, $headers);
+$comm = "Downloaded on ".date('l jS \of F Y h:i:s A')
+$zip->setArchiveComment($comm);
+$readme  = $comm.PHP_EOL;
+$readme .= "Each column's value is delimited either by a colon (CSV) or a tab (TSV).".PHP_EOL;
+//$readme .= "These are their names:".PHP_EOL;
+//$readme .= implode(PHP_EOL, $headers);
 $zip->addFromString("README", $readme);
 $zip->addFromString("logs.".$format, $dsv);
 if (!$zip->close()) {
-  die('Cannot create ZIP file.');
+  die('Cannot close ZIP file.');
 }
 // now force download
 header('Content-type: application/zip');
